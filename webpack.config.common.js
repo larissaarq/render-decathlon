37673a143c638c7e987b1083d0adf4db4328fcd3
Breno <breno.sena@decathlon.com>
2020-01-03 18:09:10 -0300

    fix: disable eslint

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const fs = require('fs')
-const webpack = require('webpack')
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -139,9 +138,9 @@ module.exports = {
               ]
             }
           },
-          {
-            loader: 'eslint-loader'
-          }
+          // {
+          //   loader: 'eslint-loader'
+          // }
         ],
         exclude: /node_modules/,
       },
