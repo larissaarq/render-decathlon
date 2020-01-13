43811e11871d8abcbd13a043a8648888970be887
Breno <breno.sena@decathlon.com>
2020-01-13 13:54:18 -0300

    feat: remove comments and duplicate css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    require('autoprefixer')({}),
+    require('postcss-discard-duplicates')({}),
+    require('postcss-discard-comments')({})
   ]
 }
